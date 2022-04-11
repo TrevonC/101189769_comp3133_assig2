b97cb5f6d6d45cb0cfcd9c10a158539c7cc1822a
TrevonC <trevon.costa@georgebrown.ca>
2022-04-10 23:00:53 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './booking/booking.component';
+import { ListingComponent } from './listing/listing.component';
+import { AddBookingComponent } from './add-booking/add-booking.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listing', pathMatch: 'full' },
+  { path: 'listing', component: ListingComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'add-booking', component: AddBookingComponent },
+  { path: 'add-listing', component: AddListingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'search', component: SearchComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BookingComponent } from './booking/booking.component';
+import { ListingComponent } from './listing/listing.component';
+import { AddBookingComponent } from './add-booking/add-booking.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
+import { SignupComponent } from './signup/signup.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BookingComponent,
+    ListingComponent,
+    AddBookingComponent,
+    AddListingComponent,
+    SignupComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
